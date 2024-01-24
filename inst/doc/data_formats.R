df4b8cdd7f226b5af6ff5e5276f9dadca13dd573
Dieter Menne <dieter.menne@menne-biomed.de>
2024-01-24 15:02:47 +0000

    version 0.8.7

--- inst/doc/data_formats.R
+++ inst/doc/data_formats.R
@@ -1,4 +1,4 @@
-## ---- echo = FALSE, include = FALSE-------------------------------------------
+## ----echo = FALSE, include = FALSE--------------------------------------------
 library(knitr)
 library(dplyr)
 library(stringr)
@@ -22,13 +22,13 @@ files = c(
  bt_df = cleanup_data(bt)
  str(bt_df)
 
-## ---- nf, fig.height = 2, fig.width =4----------------------------------------
+## ----nf, fig.height = 2, fig.width =4-----------------------------------------
  nf = null_fit(bt_df)
  str(nf)
  
  plot(nf) # dispatches to plot.breathtestfit
 
-## ---- three, fig.height = 2.5, fig.width = 8----------------------------------
+## ----three, fig.height = 2.5, fig.width = 8-----------------------------------
 files1 = c(
   group_a = btcore_file("IrisCSV.TXT"), # Use only single file with grouping
   group_a = btcore_file("Standard.TXT"),
@@ -42,7 +42,7 @@ read_any_breathtest(files1) %>%
   null_fit() %>% 
   plot()
 
-## ---- simulated,  fig.height = 5, fig.width = 6, fig.cap = "Example of a cross-over design with missing data, outliers and missing record in the red curve."----
+## ----simulated,  fig.height = 5, fig.width = 6, fig.cap = "Example of a cross-over design with missing data, outliers and missing record in the red curve."----
 set.seed(212)
 data = list(meal_a = simulate_breathtest_data(n_records = 3, noise = 2,
                           student_t_df = 3, missing = 0.3), 
@@ -53,7 +53,7 @@ data %>%
   plot()
 
 
-## ---- fig.cap= "Function simulate_breathtest_data returns the values of the parameters used to generate the data. These can be used to check the results of the model prediction."----
+## ----fig.cap= "Function simulate_breathtest_data returns the values of the parameters used to generate the data. These can be used to check the results of the model prediction."----
 data$meal_a$record
 
 ## -----------------------------------------------------------------------------
@@ -61,10 +61,10 @@ data("usz_13c")
 cat("usz_13c has data from", length(unique(usz_13c$patient_id)), "patients with" , 
     length(unique(usz_13c$group)), "different meals")
 
-## ---- fig.height = 3, include = FALSE-----------------------------------------
+## ----fig.height = 3, include = FALSE------------------------------------------
 knitr::include_graphics("breathtestshiny.png")
 
-## ---- echo = FALSE, include = FALSE-------------------------------------------
+## ----echo = FALSE, include = FALSE--------------------------------------------
 options(tibble.print_min= 4)
 options(digits = 2)
 
@@ -74,7 +74,7 @@ options(digits = 2)
 ## -----------------------------------------------------------------------------
 (cbt = cleanup_data(bt))
 
-## ---- nlsfit, height = 3, width = 4-------------------------------------------
+## ----nlsfit, height = 3, width = 4--------------------------------------------
 cbt %>% nls_fit() %>% plot()
 
 ## -----------------------------------------------------------------------------
@@ -83,40 +83,40 @@ cbt %>% nls_fit() %>% plot()
 ## -----------------------------------------------------------------------------
 (cbt = cleanup_data(bt))
 
-## ---- four_col "--------------------------------------------------------------
+## ----four_col "---------------------------------------------------------------
 bt = read_breathtest_excel(btcore_file("ExcelSamples.xlsx"), "4col_2group") %>% 
   cleanup_data()
 kable(sample_frac(bt, 0.08) %>% arrange(patient_id, group), caption = "A sample from a four-column format. See worksheet 4col_2group.")
 
 
-## ---- nlme_fit, fig.width = 7-------------------------------------------------
+## ----nlme_fit, fig.width = 7--------------------------------------------------
 bt %>% nlme_fit() %>% plot()
 
-## ---- iriswagner, fig.cap = "IRIS/Wagner composite file. These data cannot be fitted successfully with the single-curve fit method, therefore only data are shown."----
+## ----iriswagner, fig.cap = "IRIS/Wagner composite file. These data cannot be fitted successfully with the single-curve fit method, therefore only data are shown."----
 read_iris(btcore_file("IrisMulti.TXT")) %>% 
   cleanup_data() %>% 
   null_fit() %>% 
   plot()
 
-## ---- iris_csv, fig.cap = "IRIS/Wagner CSV file"------------------------------
+## ----iris_csv, fig.cap = "IRIS/Wagner CSV file"-------------------------------
 read_iris_csv(btcore_file("Standard.TXT")) %>% 
   cleanup_data() %>% 
   nls_fit() %>% 
   plot()
 
-## ---- breathidc, fig.cap = "BreathID composite file"--------------------------
+## ----breathidc, fig.cap = "BreathID composite file"---------------------------
 read_breathid(btcore_file("350_20043_0_GER.txt")) %>% 
   cleanup_data() %>% 
   nls_fit() %>% 
   plot()
 
-## ---- breathid_xml, fig.cap = "BreathID XML format"---------------------------
+## ----breathid_xml, fig.cap = "BreathID XML format"----------------------------
 read_breathid_xml(btcore_file("NewBreathID_multiple.xml")) %>% 
   cleanup_data() %>% 
   nls_fit() %>% 
   plot()
 
-## ---- breathid_man, fig.cap = "BreathID XML format with manual grouping."-----
+## ----breathid_man, fig.cap = "BreathID XML format with manual grouping."------
 # Could also use read_any_breathtest()
 read_breathid_xml(btcore_file("NewBreathID_multiple.xml")) %>% 
   cleanup_data() %>% 
--- inst/doc/methods_and_concepts.R
+++ inst/doc/methods_and_concepts.R
@@ -1,4 +1,4 @@
-## ---- echo = FALSE, include = FALSE-------------------------------------------
+## ----echo = FALSE, include = FALSE--------------------------------------------
 suppressPackageStartupMessages(library(knitr))
 suppressPackageStartupMessages(library(gridExtra))
 suppressPackageStartupMessages(library(dplyr))
@@ -9,12 +9,12 @@ opts_chunk$set(comment = NA, fig.width = 4, fig.height = 3)
 options(digits = 3)
 set.seed(4711)
 
-## ---- eval = FALSE------------------------------------------------------------
+## ----eval = FALSE-------------------------------------------------------------
 #  exp_beta = function(minute,dose,m,k,beta) {
 #       m*dose*k*beta*(1-exp(-k*minute))^(beta-1)*exp(-k*minute)
 #  }
 
-## ---- expb, fig.width = 6, echo = FALSE, fig.cap = "Examples of exponential beta functions used to fit breath test curves. _Bottom-up arrow_: Maes/Ghoos lag time. _Top-down arrow_: Maes/Ghoos half emptying time."----
+## ----expb, fig.width = 6, echo = FALSE, fig.cap = "Examples of exponential beta functions used to fit breath test curves. _Bottom-up arrow_: Maes/Ghoos lag time. _Top-down arrow_: Maes/Ghoos half emptying time."----
 k = c(0.025, 0.015)
 beta=c(1.5, 4, 8)
 g = expand.grid(minute = 0:300, k = k, beta = beta) %>% 
@@ -38,7 +38,7 @@ ggplot(g, aes(minute, pdr, color = as.character(beta))) +
   geom_segment(aes(x = tlag, y = y_tlag-10, xend = tlag, yend = y_tlag - .3), 
                data = gpar, size = 1, arrow = arrow_1) 
 
-## ---- t50, echo = FALSE, fig.cap = "The half-emptying time in the definition of Maes/Ghoos is the time where the area under curve (AUC) is half of the AUC under the curve extrapolated to infinity."----
+## ----t50, echo = FALSE, fig.cap = "The half-emptying time in the definition of Maes/Ghoos is the time where the area under curve (AUC) is half of the AUC under the curve extrapolated to infinity."----
 t50 = (gpar %>% filter(beta == 1.5 & k == 0.015 ))$t50
 
 g %>% filter(beta == 1.5 & k == 0.015 ) %>% 
@@ -49,7 +49,7 @@ g %>% filter(beta == 1.5 & k == 0.015 ) %>%
   
   
 
-## ---- gamma, fig.cap = "Gamma function from @Ghoos1993 definition as points, and fitted beta exponential as line. Both function look very similar, but the area under the gamma function is infinite and thus cannot be used to compute t50.", echo = FALSE----
+## ----gamma, fig.cap = "Gamma function from @Ghoos1993 definition as points, and fitted beta exponential as line. Both function look very similar, but the area under the gamma function is infinite and thus cannot be used to compute t50.", echo = FALSE----
 gam = function(minute, a, b, c){
   a * minute^b * exp(-c*minute)
 }
@@ -64,10 +64,10 @@ tibble(minute = seq(0, 300, by = 10)) %>%
 
 
 
-## ---- fig.height = 2.5, fig.cap="The fitting methods available in package `breathtestshiny` and in the [online version](https://apps.menne-biomed.de/breathtestshiny/).", echo = FALSE----
+## ----fig.height = 2.5, fig.cap="The fitting methods available in package `breathtestshiny` and in the [online version](https://apps.menne-biomed.de/breathtestshiny/).", echo = FALSE----
 knitr::include_graphics("methods.png")
 
-## ---- foc, fig.width = 7, fig.asp = 0.4, echo = FALSE, fig.cap="Left: pharmcological PDR response of separated loads released in the smaller bowel after 0, 30 and 90 minutes. Right: sum response when all three loads are released successively."----
+## ----foc, fig.width = 7, fig.asp = 0.4, echo = FALSE, fig.cap="Left: pharmcological PDR response of separated loads released in the smaller bowel after 0, 30 and 90 minutes. Right: sum response when all three loads are released successively."----
 foc = function(minute, t_lag, dose, clearance, k1, k2)  {
   norm = dose * k1 * k2/(clearance * (k2-k1)) 
   m_lag = pmax(minute - t_lag, 0)
@@ -89,12 +89,12 @@ p2 = ff %>%
 gridExtra::grid.arrange(p1, p2, ncol = 2, nrow = 1)
 
 
-## ---- echo = FALSE------------------------------------------------------------
+## ----echo = FALSE-------------------------------------------------------------
 tempt = 100
 beta = 1.8
 cap = paste0("Meal volume (left) and flow when gastric emptying function is a power exponential function. Slope was computed with  function `gastempt::powexp_slope()` with inverted sign. 400 ml initial value, tempt = ", tempt, ", beta = ", beta, " Half empyting times t50 are marked by arrows." )
 
-## ---- powexp, fig.height = 3, fig.width = 8, echo = FALSE, fig.cap = cap------
+## ----powexp, fig.height = 3, fig.width = 8, echo = FALSE, fig.cap = cap-------
 
 # Stolen and simplfied from package gastempt
 powexp = function(t, v0, tempt, beta){
@@ -157,7 +157,7 @@ gridExtra::grid.arrange(p1, p2, p3, ncol = 3, nrow = 1)
 
 
 
-## ---- sim2stage, fig.cap = "Simulated response of a two-stage breath test procedure. The peak within the first hour is from acetate directly released in the small bowel, the second wider peak is the result of the convolution of transport emptying and pharmacokinetics."----
+## ----sim2stage, fig.cap = "Simulated response of a two-stage breath test procedure. The peak within the first hour is from acetate directly released in the small bowel, the second wider peak is the result of the convolution of transport emptying and pharmacokinetics."----
   data.frame(pdr = c(ff_0[0:59]*50, rep(0, 20), cv$pdr)) %>% 
   mutate(
     minute = 0:(n()-1)
